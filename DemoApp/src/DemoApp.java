bac0386e3ba786ab42e1b16bdfebc6e49594b062
Rohit <rk89sah@gmail.com>
2023-10-05 21:50:57 +0530

    first commit

---
+++ DemoApp/src/DemoApp.java
@@ -0,0 +1,16 @@
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {10,20,30,40};
+		for(int ele :a) {
+			System.out.println(ele);
+		}
+		
+		String name = "sachin";
+		int count = name.toUpperCase().length();
+		System.out.println("The length of the String is: "+ count);
+	}
+
+}
